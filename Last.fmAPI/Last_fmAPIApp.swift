1f7fe09e9fc8eecd769c5e71f23829d7c809059a
Dane Shaw <danepainter1@gmail.com>
2025-10-29 23:20:41 -0400

    Initial Commit

---
+++ Last.fmAPI/Last_fmAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Last_fmAPIApp.swift
+//  Last.fmAPI
+//
+//  Created by Dane Shaw on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Last_fmAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Last.fmAPITests/Last_fmAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  Last_fmAPITests.swift
+//  Last.fmAPITests
+//
+//  Created by Dane Shaw on 10/29/25.
+//
+
+import Testing
+@testable import Last_fmAPI
+
+struct Last_fmAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
